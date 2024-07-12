1e5a2d516bf1a83abf938de54c93009465172469
ntb1911 <79504672+baobao1911@users.noreply.github.com>
2024-07-12 22:58:13 +0700

    config: edit repo on huggingface

--- main.py
+++ main.py
@@ -1,45 +1,45 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
+
 import os
-from langchain_community.llms import CTransformers
+
 from langchain.chains import RetrievalQA
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.prompts import PromptTemplate
+
+from langchain_community.llms import CTransformers
 from langchain_community.embeddings import GPT4AllEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
-import wget
+
+from huggingface_hub import hf_hub_download
+
 import tempfile
 
 app = FastAPI()
 
-model_file = 'vinallama-2.7b-chat_q5_0.gguf'
-if not os.path.isfile(model_file):
-    url = 'https://huggingface.co/vilm/vinallama-2.7b-chat-GGUF/resolve/main/vinallama-2.7b-chat_q5_0.gguf?download=true'
-    wget.download(url, 'vinallama-2.7b-chat_q5_0.gguf')
+model_file = "vinallama-2.7b-chat_q5_0.gguf"
+hf_hub_download(repo_id="bao1911/ChatWithDocs", filename=model_file)
 
 def load_llm(model_file):
-    llm = CTransformers(
+    return CTransformers(
         model=model_file,
         model_type="llama",
         max_new_tokens=1024,
         temperature=0.01
     )
-    return llm
 
 def create_prompt(template):
-    prompt = PromptTemplate(template=template, input_variables=["context", "question"])
-    return prompt
+    return PromptTemplate(template=template, input_variables=["context", "question"])
 
 def create_qa_chain(prompt, llm, db):
-    llm_chain = RetrievalQA.from_chain_type(
+    return RetrievalQA.from_chain_type(
         llm=llm,
         chain_type="stuff",
         retriever=db.as_retriever(search_kwargs={"k": 3}, max_tokens_limit=1024),
         return_source_documents=False,
         chain_type_kwargs={'prompt': prompt}
     )
-    return llm_chain
 
 template = """<|im_start|>system\nSử dụng thông tin sau đây để trả lời câu hỏi. Nếu bạn không biết câu trả lời, hãy nói không biết, đừng cố tạo ra câu trả lời\n
     {context}<|im_end|>\n<|im_start|>user\n{question}<|im_end|>\n<|im_start|>assistant"""
